7ab66e7f1c66da6b324194473c30edcbb1a61fd2
Vladimir <ryzhovvladimirr@gmail.com>
2024-05-08 10:04:47 +0500

    feat: add home page

---
+++ src/pages/styled.tsx
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const MainWrapper = styled.main`
+  padding: 15px 15px;
+`;
+
+export const MenuList = styled.ul`
+  display: flex;
+  flex-direction: column;
+  row-gap: 10px;
+`;
+
+export const MenuItem = styled.li`
+  text-align: center;
+  padding: 5px;
+
+  border: 2px solid black;
+  border-radius: 5px;
+`;
